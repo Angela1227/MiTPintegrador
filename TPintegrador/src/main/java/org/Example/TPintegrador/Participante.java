9b3fde4a3d169fbf746ac4a2b3628d041f73fd03
Angela1227 <107450699+Angela1227@users.noreply.github.com>
2023-04-24 15:51:43 -0300

    Trate de sumar lo hecho con lo que me pasaron
    Tuve una demora dado a que preparé otro TP para la facultad

--- TPintegrador/src/main/java/org/Example/TPintegrador/Participante.java
+++ TPintegrador/src/main/java/org/Example/TPintegrador/Participante.java
@@ -3,61 +3,20 @@ package org.Example.TPintegrador;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.Example.TPintegrador.App;
+
 
 public class Participante {
 	//nombre y puntaje 
-	private Equipo equipo;
-	private String nombre;
-	private Partido partido;
-	private Pronostico pronostico;
-	private int puntaje;
-	private EnumResultado resultado;
-	
 
 	
-	//para llamar al participante
-	public Participante(String nombre, int puntaje) {
-		this.nombre = nombre;
-		this.puntaje = puntaje;
-	}
-	
-	//los getter y setters
-	public String getNombre() {
-		return nombre;
-	}
-	public Equipo getEquipo() {
-		return equipo;
-	}
-	public void setEquipo(Equipo equipo) {
-		this.equipo = equipo;
-	}
-	public Partido getPartido() {
-		return partido;
-	}
-	public void setPartido(Partido partido) {
-		this.partido = partido;
-	}
-	public EnumResultado getResultado() {
-		return resultado;
-	}
-	public void setResultado(EnumResultado resultado) {
-		this.resultado = resultado;
-	}
-	
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
 	
 	
-	//establezco el puntaje
-	public  int getPuntaje() {
-		return puntaje;
-	}
-	//establezo que puntaje se relaciona con puntos
-	public void setPuntaje(int puntaje) {
-		this.puntaje = pronostico.puntos();
-	}
+	 }
 	
 	
 	
@@ -68,4 +27,4 @@ public class Participante {
 		
 		
 
-}
+
